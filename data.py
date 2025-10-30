1fdc2613aa6c2f7cc4ba1f6f0de7bce5787076ec
FeluciaPS <TheMezStrikes@gmail.com>
2025-10-30 15:07:57 +0100

    Banish lists to data.py

---
+++ data.py
@@ -0,0 +1,81 @@
+
+
+RACE_NAMES = [
+    "City Slicker",
+    "Mally Market",
+    "Play Misty For Me",
+    "Prohibited Site",
+    "Catz In The Hood",
+    "Spooky Manor",
+    "Pastacosi Factory",
+    "Caskou Park",
+    "Palerock Lake",
+    "Country Bumpkin",
+    "Sneak-A-Peak",
+    "Loopy Lagoon",
+    "Crazy Dunes",
+    "Valley Of The Kings",
+    "Blazing Oasis",
+    "Mysterious Temple",
+]
+
+CUP_NAMES = [
+    "Lasagna Cup",
+    "Pizza Cup",
+    "Burger Cup",
+    "Ice Cream Cup",
+]
+
+CHARACTER_NAMES = [
+    "Garfield",
+    "Jon",
+    "Liz",
+    "Odie", 
+    "Arlene",
+    "Nermal",
+    "Squeak",
+    "Harry",
+]
+
+CAR_NAMES = [
+    "Formula Zzzz",
+    "Abstract-Kart",
+    "Medi-Kart",
+    "Woof-Mobile",
+    "Kissy-Kart",
+    "Cutie-Pie Cat",
+    "Rat-Racer",
+    "Muck-Madness",
+]
+
+HAT_NAMES = [
+    "Beddy-Bye Cap",
+    "Whizzy Wizard",
+    "Tic-Toque",
+    "Elasto-Hat",
+    "Chef's Special",
+    "Cutie-Pie Crown",
+    "Viking Helmet",
+    "Stink-O-Rama",
+
+    # Unique character hats
+    "Space Bubble", # Garfield
+    "Pizzaiolo Hat", # Jon
+    "Bunny Band", # Liz
+    "Joe Montagna", # Odie
+    "Aristo-Catic Bicorn", # Arlene
+    "Toutankhameow", # Nermal
+    "Apprentice Sorcerer", # Squeak
+    "Mule Head", # Harry
+]
+
+SPOILER_NAMES = [
+    "Bombastic Spoiler",
+    "Whacky Spoiler",
+    "Superfit Spoiler",
+    "Cyclobone Spoiler",
+    "Foxy Spoiler",
+    "Shimmering Spoiler",
+    "Holey Moley Spoiler",
+    "Stained Spoiler",
+]
