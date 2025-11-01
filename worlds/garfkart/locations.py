cedbded5eee147a0a6e387adf1f38fa2ffe1e6e9
FeluciaPS <TheMezStrikes@gmail.com>
2025-11-01 12:22:55 +0100

    v0.1 technically generates

--- worlds/garfkart/locations.py
+++ worlds/garfkart/locations.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 
 
 # This file defines all locations that contain items.
@@ -7,3 +8,47 @@
 # - Beat Cup 3
 # - Beat Cup 4
 # (Yes, I can't be bothered to look up the cup names right now)
+
+from BaseClasses import Location
+from . import items
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .world import GarfKartWorld
+
+from .data import CUP_NAMES
+
+# Cup victories reserve IDs 100-150.
+
+CUP_LOCATION_TABLE = {}
+
+for index, cup in enumerate(CUP_NAMES):
+    name = f'Win {cup}'
+    CUP_LOCATION_TABLE[name] = index + 101
+
+LOCATION_NAME_TO_ID = {
+    **CUP_LOCATION_TABLE
+}
+
+class GarfKartLocation(Location):
+    game = "Garfield Kart - Furious Racing"
+
+# Copied from APQuest and now's not the time for me to figure out how it works or why
+def get_location_names_with_ids(location_names: list[str]) -> dict[str, int | None]:
+    return {location_name: LOCATION_NAME_TO_ID[location_name] for location_name in location_names}
+
+def create_all_locations(world: GarfKartWorld) -> None:
+    create_regular_locations(world)
+    create_events(world)
+
+def create_regular_locations(world: GarfKartWorld) -> None:
+    # Add cup victory locations
+    for cup in CUP_NAMES:
+        location_data = get_location_names_with_ids([f"Win {cup}"])
+        region = world.get_region(cup)
+        region.add_locations(location_data, GarfKartLocation)
+
+def create_events(world: GarfKartWorld) -> None:
+    for cup in CUP_NAMES:
+        region = world.get_region(cup)
+        region.add_event(f"{cup} Victory", f"{cup} Victory", location_type=GarfKartLocation, item_type=items.GarfKartItem)
--- worlds/garfkart/world.py
+++ worlds/garfkart/world.py
@@ -1,8 +1,14 @@
+
+
+from collections.abc import Mapping
+from typing import Any
 from worlds.AutoWorld import World
+
+from . import items, locations, options, regions, rules, web_world
 
 # I'll be calling these classes GarfKart because GarfieldKartFuriousRacing
 # is way too long and I don't believe for a second anyone's going to
-# randomise the original Garfield Kart, so there shouldn't be an issue
+# randomize the original Garfield Kart, so there shouldn't be an issue
 # using that class name.
 class GarfKartWorld(World):
     """
@@ -11,3 +17,35 @@ class GarfKartWorld(World):
     """
 
     game = "Garfield Kart - Furious Racing"
+
+    web = web_world.GarfKartWebWorld()
+
+    options_dataclass = options.GarfKartOptions
+    options: options.GarfKartOptions
+
+    location_name_to_id = locations.LOCATION_NAME_TO_ID
+    item_name_to_id = items.ITEM_NAME_TO_ID
+
+    origin_region_name = "Menu"
+
+    def create_regions(self) -> None:
+        regions.create_and_connect_regions(self)
+        locations.create_all_locations(self)
+
+    def set_rules(self) -> None:
+        rules.set_all_rules(self)
+
+    def create_items(self) -> None:
+        items.create_all_items(self)
+
+    def create_item(self, name: str) -> items.GarfKartItem:
+        return items.create_item_object(self, name)
+    
+    def get_filler_item_name(self) -> str:
+        return items.get_random_filler_item(self)
+    
+    # Copied from APQuest for the time being
+    def fill_slot_data(self) -> Mapping[str, Any]:
+        return self.options.as_dict(
+            "goal", "progressive_cups"
+        )
