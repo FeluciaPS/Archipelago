305f0d1ea153d5807cc7509dee3bc604741d10a6
FeluciaPS <TheMezStrikes@gmail.com>
2025-11-05 19:26:53 +0100

    Add race randomization to item and location pool

--- worlds/garfkart/locations.py
+++ worlds/garfkart/locations.py
@@ -109,12 +109,20 @@ def create_all_locations(world: GarfKartWorld) -> None:
     create_events(world)
 
 def create_regular_locations(world: GarfKartWorld) -> None:
+    randomize_races = world.options.randomize_races == "races" or world.options.randomize_races == "cups_and_races"
+    randomize_cups = world.options.randomize_races == "cups" or world.options.randomize_races == "cups_and_races"
 
     # Add cup victory locations
-    # TODO: I don't like the "or" necessary here, I'll think about it... -Felucia
-    if world.options.randomize_races == "cups" or world.options.randomize_races == "cups_and_races":
+    if randomize_cups:
         for cup in CUP_NAMES:
             location_data = get_location_names_with_ids([f"{cup}: Victory"])
+            region = world.get_region("Menu")
+            region.add_locations(location_data, GarfKartLocation)
+
+    # Add race victory locations
+    if randomize_races:
+        for race in RACE_NAMES:
+            location_data = get_location_names_with_ids([f"{race}: Victory"])
             region = world.get_region("Menu")
             region.add_locations(location_data, GarfKartLocation)
 
